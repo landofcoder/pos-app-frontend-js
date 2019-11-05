89e0c5c6fb9d355bcc3ee4374618e146ad8dd228
chienvu <fchienvuhoang@gmail.com>
2019-11-05 10:13:02 +0700

    - Add ui style

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListCart from './Cart/ListCart';
 import routes from '../constants/routes';
+import Styles from './Home.scss';
+import CommonStyle from './common.scss';
 
 type Props = {
   productList: Array,
@@ -15,80 +17,85 @@ export default class Home extends Component<Props> {
   render() {
     const { productList, addToCart } = this.props;
     return (
-      <div data-tid="container">
-        <div className="row pt-4">
-          <div className="col-md-8">
-            <div className="col-md-12 mb-4">
-              <div className="input-group flex-nowrap">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="addon-wrapping">
-                    Search
-                  </span>
+      <>
+        <div data-tid="container">
+          <div className="row pt-4">
+            <div className="col-md-9">
+              <div className="col-md-12 mb-4">
+                <div className="input-group flex-nowrap">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text" id="addon-wrapping">
+                      Search
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="name, sku"
+                    aria-label="Username"
+                    aria-describedby="addon-wrapping"
+                  />
                 </div>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="name, sku"
-                  aria-label="Username"
-                  aria-describedby="addon-wrapping"
-                />
+              </div>
+              <div className="row">
+                {productList.map(item => (
+                  <div className="col-md-4 mb-4" key={item.id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">
+                          Card subtitle
+                        </h6>
+                        <p className="card-text">
+                          Some quick example text to build on the card title and
+                          make up make up make up the bulk of the card content.
+                        </p>
+                        <a
+                          href="#"
+                          className="card-link"
+                          onClick={() => addToCart(item)}
+                        >
+                          Add to cart
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-            <div className="row">
-              {productList.map(item => (
-                <div className="col-md-4 mb-4" key={item.id}>
-                  <div className="card">
-                    <div className="card-body">
-                      <h5 className="card-title">Card title</h5>
-                      <h6 className="card-subtitle mb-2 text-muted">
-                        Card subtitle
-                      </h6>
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up make up make up the bulk of the card content.
-                      </p>
-                      <a
-                        href="#"
-                        className="card-link"
-                        onClick={() => addToCart(item)}
-                      >
-                        Add to cart
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card">
-              <div className="card-body">
-                <ListCart />
-                <div className="mt-4">
-                  <div className="row">
-                    <div className="col-md-4 col-xs-4">
-                      <a
-                        href="#"
-                        className="btn btn-secondary btn-lg btn-block"
-                      >
-                        Hold
-                      </a>
-                    </div>
-                    <div className="col-md-8 col-xs-8">
-                      <Link
-                        className="btn btn-primary btn-lg btn-block"
-                        to={routes.CHECKOUT}
-                      >
-                        Checkout
-                      </Link>
-                    </div>
-                  </div>
+            <div className="col-md-3">
+              <div className={CommonStyle.wrapLevel1}>
+                <div className={CommonStyle.wrapCartPanelPosition}>
+                  <ListCart />
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className={Styles.wrapFooterAction}>
+          <div className={Styles.wrapAction}>
+            <div className="col-md-2 pr-0">
+              <Link className="btn btn-secondary btn-lg btn-block">Hold</Link>
+            </div>
+            <div className="col-md-2">
+              <Link
+                className="btn btn-danger btn-lg btn-block"
+                to={routes.CHECKOUT}
+              >
+                Empty cart
+              </Link>
+            </div>
+            <div className="col-md-3 pl-0 pr-0">
+              <Link
+                className="btn btn-primary btn-lg btn-block"
+                to={routes.CHECKOUT}
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
