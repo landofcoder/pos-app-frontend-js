bdb77eaf0464ed4bc0601c112cfa1a7b918f719e
truonghdpk <truongondaihoc@gmail.com>
2019-11-06 16:08:21 +0700

    UIcash

--- app/components/payment/Cash/Cash.js
+++ app/components/payment/Cash/Cash.js
@@ -6,6 +6,53 @@ export default class CashPayment extends Component<Props> {
   props: Props;
 
   render() {
-    return <div className={styles.wrapCashPayment}>CASH COMPONENT</div>;
+    return (
+      <div className={`${styles.wrapCashPayment}`}>
+        <div className={styles.center}>
+          <i className={`fa fa-money-bill-wave fa-3x ${styles.dollar}`} />
+          <br />
+          <span className={`${styles.colorText}`}>Total</span>
+          <br />
+          <span className={`${styles.size}`}>$233.53</span>
+          <br />
+          <div className={`${styles.tableMount} row`}>
+            <span className={`${styles.colorText} ${styles.paddingLeft}`}>
+              Amount
+            </span>
+            <span className={`${styles.size} ${styles.paddingRight}`}>
+              $233.53
+            </span>
+          </div>
+          <button type="button" className="btn btn-lg btn-secondary">
+            $233.53
+          </button>
+          <button type="button" className="btn btn-lg btn-secondary">
+            $250.00
+          </button>
+          <button type="button" className="btn btn-lg btn-secondary">
+            $260.00
+          </button>
+          <button type="button" className="btn btn-lg btn-secondary">
+            $300.00
+          </button>
+        </div>
+        <div className={styles.footerButton}>
+          <div className="col-md-2 pr-0">
+            <button
+              type="button"
+              className="btn btn-lg btn-secondary btn-block"
+            >
+              Back
+            </button>
+          </div>
+
+          <div className="col-md-3 pr-0">
+            <button type="button" className="btn btn-lg btn-primary btn-block">
+              Accept
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
