0a1c29294fe17ba56dde93e319ae0164d94c4fba
truonghdpk <truongondaihoc@gmail.com>
2019-11-07 11:10:49 +0700

    UI cash

--- app/components/payment/Cash/Cash.js
+++ app/components/payment/Cash/Cash.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import commonStyle from '../../common.scss';
 import styles from './cash.scss';
 
 export default class CashPayment extends Component<Props> {
@@ -7,36 +8,50 @@ export default class CashPayment extends Component<Props> {
 
   render() {
     return (
-      <div className={`${styles.wrapCashPayment}`}>
-        <div className={styles.center}>
-          <i className={`fa fa-money-bill-wave fa-3x ${styles.dollar}`} />
+      <div className={`${commonStyle.wrapCashPayment}`}>
+        <div className={commonStyle.wrapCenterCash}>
+          <i
+            className={`fa fa-money-bill-wave fa-3x ${commonStyle.headerIcon}`}
+          />
           <br />
-          <span className={`${styles.colorText}`}>Total</span>
+          <span className={`${commonStyle.colorText}`}>Total</span>
           <br />
-          <span className={`${styles.size}`}>$233.53</span>
+          <span className={`${commonStyle.sizeFont}`}>$233.53</span>
           <br />
-          <div className={`${styles.tableMount} row`}>
-            <span className={`${styles.colorText} ${styles.paddingLeft}`}>
+          <div className={`${styles.wrapTable} row`}>
+            <span className={`${commonStyle.colorText} ${styles.forLeft}`}>
               Amount
             </span>
-            <span className={`${styles.size} ${styles.paddingRight}`}>
+            <span className={`${commonStyle.sizeFont} ${styles.forRight}`}>
               $233.53
             </span>
           </div>
-          <button type="button" className="btn btn-lg btn-secondary">
+          <button
+            type="button"
+            className={`btn btn-lg btn-secondary ${commonStyle.buttonBody}`}
+          >
             $233.53
           </button>
-          <button type="button" className="btn btn-lg btn-secondary">
+          <button
+            type="button"
+            className={`btn btn-lg btn-secondary ${commonStyle.buttonBody}`}
+          >
             $250.00
           </button>
-          <button type="button" className="btn btn-lg btn-secondary">
+          <button
+            type="button"
+            className={`btn btn-lg btn-secondary ${commonStyle.buttonBody}`}
+          >
             $260.00
           </button>
-          <button type="button" className="btn btn-lg btn-secondary">
+          <button
+            type="button"
+            className={`btn btn-lg btn-secondary ${commonStyle.buttonBody}`}
+          >
             $300.00
           </button>
         </div>
-        <div className={styles.footerButton}>
+        <div className={commonStyle.footerButton}>
           <div className="col-md-2 pr-0">
             <button
               type="button"
