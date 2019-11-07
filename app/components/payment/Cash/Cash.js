d18b9d8d5622ba357b406ab5e09bddd66b8eab47
Chien <chien@autoketing.com>
2019-11-07 18:20:32 +0700

    - Add main panel type for switch main panel

--- app/components/payment/Cash/Cash.js
+++ app/components/payment/Cash/Cash.js
@@ -1,11 +1,45 @@
 // @flow
 import React, { Component } from 'react';
-import styles from './cash.scss';
 
-export default class CashPayment extends Component<Props> {
+import { connect } from 'react-redux';
+
+type Props = {
+  cashLoadingPreparingOrder: boolean,
+  orderPreparingCheckout: Object
+};
+
+class CashPayment extends Component<Props> {
   props: Props;
 
   render() {
-    return <div className={styles.wrapCashPayment}>CASH COMPONENT</div>;
+    const { cashLoadingPreparingOrder, orderPreparingCheckout } = this.props;
+    console.log('order preparing checkout:', orderPreparingCheckout);
+    return (
+      <div className="col-md-3">
+        <button type="button" className="btn btn-primary btn-lg">
+          {cashLoadingPreparingOrder ? (
+            <span
+              className="spinner-border"
+              role="status"
+              aria-hidden="true"
+            ></span>
+          ) : (
+            <>{orderPreparingCheckout.totals.grand_total}</>
+          )}
+        </button>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    cashLoadingPreparingOrder: state.mainRd.cashLoadingPreparingOrder,
+    orderPreparingCheckout: state.mainRd.orderPreparingCheckout
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(CashPayment);
