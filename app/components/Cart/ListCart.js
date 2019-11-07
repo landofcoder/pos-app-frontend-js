77891a295ebcf1627b2cb75c26fcf5ac7a604120
truonghdpk <truongondaihoc@gmail.com>
2019-11-07 09:35:13 +0700

    UI Cart

--- app/components/Cart/ListCart.js
+++ app/components/Cart/ListCart.js
@@ -1,24 +1,45 @@
 // @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styles from './listcart.scss';
 
 type Props = {
-  cartCurrent: Array
+  //  cartCurrent: Array
 };
 
 class ListCart extends Component<Props> {
   props: Props;
 
   render() {
-    const { cartCurrent } = this.props;
+    //  const { cartCurrent } = this.props;
     return (
       <div>
         <ul className="list-group">
-          {cartCurrent.data.map(item => (
-            <li key={item.id} className="list-group-item">
-              Cras justo odio
-            </li>
-          ))}
+          <li className={`list-group-item ${styles.item}`}>
+            <div className="row">
+              <div className="col-sm-3">
+                <img
+                  src="https://images-na.ssl-images-amazon.com/images/I/81SHf40Ph3L._UL1500_.jpg"
+                  alt=""
+                />
+              </div>
+              <div className={`col-sm-6 p-0 ${styles.title}`}>
+                <span>Bolo Sport Watch</span>
+                <br />
+                <span className={styles.blockquote}>another thing</span>
+              </div>
+              <div className={`col-sm-2 p-0 ${styles.cost}`}>
+                <div>
+                  <span>$23.00</span>
+                </div>
+              </div>
+              <div className={`col-sm-1 p-0 ${styles.cancel}`}>
+                <div>
+                  <i className={`far fa-times-circle ${styles.icon}`}></i>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     );
