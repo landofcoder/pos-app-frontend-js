9bea15daea3eadc732313661b0917a35d3742807
Chien <chien@autoketing.com>
2019-11-08 14:53:42 +0700

    - Improve code

--- app/components/Cart/ListCart.js
+++ app/components/Cart/ListCart.js
@@ -16,38 +16,34 @@ class ListCart extends Component<Props> {
       <div>
         <ul className="list-group">
           {cartCurrent.data.map(item => (
-            <li key={item.id} className="list-group-item">
-              Cras justo odio
-            </li>
-          ))}
-
-          <li className={`list-group-item ${styles.item}`}>
-            <div className={`row ${styles.tableFlex}`}>
-              <div className="col-md-3 pr-5 pb-2">
-                <img
-                  className={styles.sizeimgsmall}
-                  src="https://images-na.ssl-images-amazon.com/images/I/81SHf40Ph3L._UL1500_.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={`col-md-9 pr-5 row ${styles.wrapContent}`}>
-                <div className={`col-md-9 ${styles.title}`}>
-                  <span>Bolo Sport Watch</span>
-                  <span className={styles.blockquote}>another thing</span>
+            <li key={item.id} className={`list-group-item ${styles.item}`}>
+              <div className={`row ${styles.tableFlex}`}>
+                <div className="col-md-3 pr-5 pb-2">
+                  <img
+                    className={styles.sizeimgsmall}
+                    src="https://images-na.ssl-images-amazon.com/images/I/81SHf40Ph3L._UL1500_.jpg"
+                    alt=""
+                  />
                 </div>
-                <div
-                  className={`col-md-3 pr-5 ${styles.spaceTable} ${styles.cost}`}
-                >
-                  <div>
-                    <span>$23.00</span>
+                <div className={`col-md-9 pr-5 row ${styles.wrapContent}`}>
+                  <div className={`col-md-9 ${styles.title}`}>
+                    <span>Bolo Sport Watch</span>
+                    <span className={styles.blockquote}>another thing</span>
+                  </div>
+                  <div
+                    className={`col-md-3 pr-5 ${styles.spaceTable} ${styles.cost}`}
+                  >
+                    <div>
+                      <span>$23.00</span>
+                    </div>
                   </div>
                 </div>
+                <div className={`p-0 ${styles.cancel}`}>
+                  <i className={`far fa-times-circle ${styles.icon}`}></i>
+                </div>
               </div>
-              <div className={`p-0 ${styles.cancel}`}>
-                <i className={`far fa-times-circle ${styles.icon}`}></i>
-              </div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
     );
