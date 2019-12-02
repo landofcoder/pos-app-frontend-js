4a3cdf9c6b70ac177dab170551aea6e6784e51dc
chien <fchienvuhoang@gmail.com>
2019-12-02 17:46:06 +0700

    Remove old project

--- app/components/Account/Account.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Account extends Component<Props> {
-  props: Props;
-
-  render() {
-    return <div>Account</div>;
-  }
-}
--- app/reducers/common.js
+++
@@ -1,7 +0,0 @@
-export const cartCurrentDefaultData = {
-  cartId: '',
-  customerToken: '',
-  data: [],
-  customer: null, // Current customer for current cart
-  isGuestCustomer: true
-};
