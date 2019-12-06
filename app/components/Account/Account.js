e1635292c7d7db2375ae9442adc9912770d35f06
Truong <nvcntg@gmail.com>
2019-12-06 17:38:23 +0700

    update layout Account

--- app/components/Account/Account.js
+++ app/components/Account/Account.js
@@ -1,74 +1,79 @@
 import React, { Component } from 'react';
-import Styles from './Account.scss';
 import { Link } from 'react-router-dom';
 import * as routes from '../../constants/routes.json';
 import OrderHistory from './OrderHistory/OrderHistory';
+import CashierInfo from './CashierInfo/CashierInfo';
+import Styles from './Account.scss';
+
 export default class Account extends Component<Props> {
   props: Props;
+
   constructor(props) {
     super(props);
-    this.state = { cashierSelect: 'active', orderHistorySelect: '' };
+    this.state = { cashierSelect: '', orderHistorySelect: 'active' };
   }
+
   cashierAction = e => {
     e.preventDefault();
     this.setState({ cashierSelect: 'active', orderHistorySelect: '' });
   };
+
   orderHistoryAction = e => {
     e.preventDefault();
     this.setState({ orderHistorySelect: 'active', cashierSelect: '' });
   };
+
   render() {
     const { cashierSelect, orderHistorySelect } = this.state;
-    console.log('cashier select : ' + cashierSelect);
-    const classWrapProductPanel = `pr-3 ${Styles.wrapProductPanel} row`;
+    console.log(`cashier select : ${cashierSelect}`);
     return (
       <>
-        <div className="col-md-1 pl-0 pr-1">
-          <Link
-            className="btn btn-outline-secondary btn-lg btn-block"
-            to={routes.HOME}
-          >
-            HOME
-          </Link>
-        </div>
-        <div
-          data-tid="container"
-          className={`${Styles.wrapFullHeight} pr-0 pl-0`}
-        >
-          <div className="row">
-            <div className="col-3">
-              <div
-                className="nav flex-column nav-pills"
-                id="v-pills-tab"
-                role="tablist"
-                aria-orientation="vertical"
-              >
-                <a
-                  className={`nav-link ${cashierSelect}`}
-                  id="v-pills-home-tab"
-                  data-toggle="pill"
-                  href="#v-pills-home"
-                  role="tab"
-                  aria-controls="v-pills-home"
-                  onClick={this.cashierAction}
+        <div data-tid="container" className="pr-0 pl-0 pt-2">
+          <div className={`${Styles.wrapFullHeight}`}>
+            <div className="row">
+              <div className="col-3">
+                <div className="flex-column col-md-1 pl-0 pr-1 mt-2 mb-2">
+                  <Link to={routes.HOME}>
+                    <i
+                      className={`fas fa-arrow-left fa-1x pl-3 ${Styles.greyColor}`}
+                    ></i>
+                  </Link>
+                </div>
+                <div
+                  className="card nav flex-column nav-pills ml-2"
+                  id="v-pills-tab"
+                  role="tablist"
+                  aria-orientation="vertical"
                 >
-                  Cashier
-                </a>
-                <a
-                  className={`nav-link ${orderHistorySelect}`}
-                  id="v-pills-profile-tab"
-                  data-toggle="pill"
-                  href="#v-pills-profile"
-                  role="tab"
-                  aria-controls="v-pills-profile"
-                  onClick={this.orderHistoryAction}
-                >
-                  Order History
-                </a>
+                  <div className="card-header">Account Setting</div>
+                  <a
+                    className={`nav-link ${orderHistorySelect} ${Styles.radiusButton}`}
+                    id="v-pills-profile-tab"
+                    data-toggle="pill"
+                    href="#v-pills-profile"
+                    role="tab"
+                    aria-controls="v-pills-profile"
+                    onClick={this.orderHistoryAction}
+                  >
+                    Order History
+                  </a>
+                  <a
+                    className={`nav-link ${cashierSelect} ${Styles.radiusButton}`}
+                    id="v-pills-home-tab"
+                    data-toggle="pill"
+                    href="#v-pills-home"
+                    role="tab"
+                    aria-controls="v-pills-home"
+                    onClick={this.cashierAction}
+                  >
+                    Cashier
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="col-9">
-              <OrderHistory />
+              <div className="col-9">
+                {orderHistorySelect ? <OrderHistory /> : null}
+                {cashierSelect ? <CashierInfo /> : null}
+              </div>
             </div>
           </div>
         </div>
---
+++ app/components/Account/CashierInfo/CashierInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class CashierInfo extends Component {
+  render() {
+    return (
+      <>
+        <div className="container-fluid mt-4">
+          <h5 className="card-title">Cashier Information</h5>
+        </div>
+        <div className="form-group">
+          <h4>Email Adrdress</h4>
+        </div>
+      </>
+    );
+  }
+}
