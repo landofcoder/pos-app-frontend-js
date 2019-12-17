3a7a8bde02c4aecf09eeb3df78e1549eaab8791b
fchienvuhoang <fchienvuhoang@gmail.com>
2019-12-17 16:37:26 +0700

    Remove all master files

--- app/actions/accountAction.js
+++
@@ -1,7 +0,0 @@
-import * as type from '../constants/root.json';
-
-export function getOrderHistory() {
-  return {
-    type: type.GET_ORDER_HISTORY_ACTION
-  };
-}
--- app/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-import rootSaga from './rootSaga';
-import authenSaga from './authenSaga';
-
-function* saga() {
-  yield all([rootSaga(), authenSaga()]);
-}
-
-export default saga;
