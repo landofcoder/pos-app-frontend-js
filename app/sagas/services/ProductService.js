6c9f51cb345547a8e41287195970f61bb316a9f1
fchienvuhoang <fchienvuhoang@gmail.com>
2019-12-25 17:26:06 +0700

    Update code

--- app/sagas/services/ProductService.js
+++ app/sagas/services/ProductService.js
@@ -511,6 +511,10 @@ async function syncAllProductsByCategory(categoryId, category = null) {
   // Let all parents categories of this category
   const defaultCategory = await getCategories();
   const allParentIds = await findAllParentCategories(defaultCategory[0].children_data, categoryId);
+  console.log('category id', categoryId);
+  if(categoryId === 8) {
+    console.log('matched:', allParentIds);
+  }
 
   // Sync products
   const totalCount = productsResult.totalCount;
