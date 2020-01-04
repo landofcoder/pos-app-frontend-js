dc69b75b19e989f7f759446464b1d9486528c671
Truong <nvcntg@gmail.com>
2020-01-04 09:57:03 +0700

    disable log

--- app/reducers/db/db.js
+++ app/reducers/db/db.js
@@ -6,7 +6,7 @@ db.version(version).stores({
   products: '++id, *categoryIds',
   categories: '++id',
   customers: '++id',
-  signUpCustomers: '++id,*username',
+  sign_up_customers: '++id,*email',
   settings: '++id, *key, created_at, updated_at'
 });
 
--- app/reducers/db/signUpCustomers.js
+++ app/reducers/db/signUpCustomers.js
@@ -3,13 +3,12 @@ import db from './db';
 export async function signUpCustomer(customers) {
   const { payload } = customers;
   const data = {
-    username: payload.customer.email,
+    email: payload.customer.email,
     payload
   };
-  const signUpCustomerTbl = db.table('signUpCustomers');
-  console.log(payload.customer.email);
+  const signUpCustomerTbl = db.table('sign_up_customers');
   const customer = await signUpCustomerTbl.get({
-    username: payload.customer.email
+    email: payload.customer.email
   });
   if (customer) {
     await signUpCustomerTbl.update(customer.id, data);
