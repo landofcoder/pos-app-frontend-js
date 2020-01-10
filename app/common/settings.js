79e793cbaa482fa6a1d7950c1db670a140a4e941
Truong <nvcntg@gmail.com>
2020-01-10 14:03:57 +0700

    fix valid input workplace

--- app/common/settings.js
+++ app/common/settings.js
@@ -17,7 +17,8 @@ export function checkValidateUrlLink(
   const url = defaultProtocol + mainUrl + lastUrlRequired;
   if (mainUrl.length < 3) return false;
   if (url.lastIndexOf('http')) return false;
-  if (url.indexOf(" ") !== -1) return false;
+  if (url.indexOf(' ') !== -1) return false;
+  if (url[url.length - 2] === '.') return false;
   try {
     new URL(url);
   } catch (_) {
