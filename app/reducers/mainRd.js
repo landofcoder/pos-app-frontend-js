47baf20e8a907090b2c1478250797b77e3f4dedf
truong <truongondaihoc@gmail.com>
2020-03-09 09:50:10 +0700

    disable log test

--- app/reducers/mainRd.js
+++ app/reducers/mainRd.js
@@ -483,16 +483,6 @@ const mainRd = (state: Object = initialState, action: Object) =>
         break;
       case types.UPDATE_SWITCHING_MODE:
         // i found a bug in this case that switchingMode in LOGIN_FORM after sync success it auto switch to HOME_PAGE :/
-        console.log('switchingMode before');
-        console.log(state.switchingMode);
-        console.log('payload after');
-        console.log(action.payload);
-        console.log('get offline mode');
-        if (state.switchingMode === 'LOGIN_FORM') {
-          console.log(getOfflineMode());
-        }
-        console.log('special condition');
-        console.log(state.specialConditionSwitchMode);
         if (
           !(
             state.switchingMode === LOGIN_FORM &&
