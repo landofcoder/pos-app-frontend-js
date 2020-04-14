36961dc8c50cabeea0e2cb7161daf7e0ff56bf56
Chiến <fchienvuhoang@gmail.com>
2020-04-14 16:58:05 +0700

    - Stringify discountCode param when call get-discount-quote api

--- app/sagas/services/cart-service.js
+++ app/sagas/services/cart-service.js
@@ -323,7 +323,7 @@ export async function getDiscountForQuoteService(payload) {
         redirect: 'follow',
         referrer: 'no-referrer',
         body: JSON.stringify({
-          params: JSON.stringify({ cart, customerId, discountCode })
+          params: JSON.stringify({ cart, customerId, discountCode: JSON.stringify(discountCode) })
         })
       }
     );
