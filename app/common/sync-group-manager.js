38fc9b179e8f1adfe95fc6b524dcf2fc0d7e9c4e
truong <truongondaihoc@gmail.com>
2020-05-09 08:42:49 +0700

    update error default

--- app/common/sync-group-manager.js
+++ app/common/sync-group-manager.js
@@ -2,6 +2,6 @@ export function serviceTypeGroupManager(serviceName, actionService) {
   // actionService is be object not an array
   return {
     name: serviceName,
-    errors: actionService.errors
+    errors: actionService.errors || 1
   };
 }
