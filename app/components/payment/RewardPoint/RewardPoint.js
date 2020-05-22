956717d1a6b35cfb78d38657d1903f6478bc406a
Chiến <fchienvuhoang@gmail.com>
2020-05-22 12:42:41 +0700

    - Build mini box reward point

--- app/components/payment/RewardPoint/RewardPoint.js
+++ app/components/payment/RewardPoint/RewardPoint.js
@@ -19,7 +19,7 @@ class RewardPoint extends Component<Props> {
               <div className="card-body">
                 <h5 className="card-title">Reward point</h5>
                 {isLoadingRewardPointInfo ? (
-                  <div className="d-flex justify-content-center">
+                  <div className="d-flex justify-content-center mb-2">
                     <div
                       className="spinner-border spinner-border-sm text-secondary"
                       role="status"
@@ -30,9 +30,23 @@ class RewardPoint extends Component<Props> {
                 ) : (
                   <></>
                 )}
-                <Slider />
+                <div className="form-group">
+                  <select className="custom-select custom-select-sm">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  <div className="mt-3">
+                    <Slider />
+                  </div>
+                </div>
                 <div className="mt-3 text-right">
-                  <button disabled type="button" className="btn btn-outline-dark btn-sm">
+                  <button
+                    disabled
+                    type="button"
+                    className="btn btn-outline-dark btn-sm"
+                  >
                     Apply
                   </button>
                 </div>
