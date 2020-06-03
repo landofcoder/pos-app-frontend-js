92474475dc158320160c8fb83edfdfd8bf77dc8f
Chiến <fchienvuhoang@gmail.com>
2020-06-03 13:27:39 +0700

    - Added stock detail

--- app/components/commons/StockDisplay/StockDisplay.js
+++ app/components/commons/StockDisplay/StockDisplay.js
@@ -3,12 +3,31 @@ import { connect } from 'react-redux';
 import Styles from '../../pos.scss';
 import Stock from '../stock';
 import { getProductByCategory } from '../../../actions/homeAction';
+import ModalStyle from '../../styles/modal.scss';
 
 class StockDisplay extends Component {
   props: Props;
 
+  state = {
+    stockDetailOpen: false
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keydown', this.escFunction, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.escFunction, false);
+  }
+
+  escFunction = event => {
+    if (event.keyCode === 27) {
+      this.setState({ stockDetailOpen: false });
+    }
+  };
+
   showStockDetail = () => {
-    console.log('show stock detail');
+    this.setState({ stockDetailOpen: true });
   };
 
   getInventoryByOutletWareHouse(stock) {
@@ -27,8 +46,45 @@ class StockDisplay extends Component {
 
   render() {
     const { stockItem } = this.props;
+    const stockList = stockItem.stock ? stockItem.stock : [];
+    const { stockDetailOpen } = this.state;
     return (
       <>
+        <div
+          className={ModalStyle.modal}
+          id="payModal"
+          style={{ display: stockDetailOpen ? 'block' : 'none' }}
+        >
+          <div className={ModalStyle.modalContent}>
+            <div className={ModalStyle.close}>
+              Close
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Stock inventory</h5>
+                <ul className="list-group">
+                  {stockList.map((item, index) => {
+                    const itemAssign = JSON.parse(item);
+                    return (
+                      <li
+                        key={index}
+                        className="list-group-item d-flex justify-content-between align-items-center"
+                      >
+                        {itemAssign.code}
+                        <span className="badge badge-primary badge-pill">
+                          {itemAssign.total_qty
+                            ? itemAssign.total_qty
+                            : itemAssign.quantity}
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div
           role="presentation"
           className={`${Styles.wrapStock}`}
